707438434cbb635b6e5170fd8797f4e13f938009
jibin-dev <jibinsebastian008@gmail.com>
2023-10-26 11:14:58 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:amazone/Screens/sign_in_screen.dart';
+import 'package:amazone/utils/color_themes.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb){
+    await Firebase.initializeApp(
+      
+    );
+  }
+
+  runApp(AmazoneClone());
+}
+class AmazoneClone extends StatelessWidget {
+  const AmazoneClone({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light().copyWith(
+        scaffoldBackgroundColor: backgroundColor
+      ),
+      home: SignInScreen(),
+    );
+  }
+}
